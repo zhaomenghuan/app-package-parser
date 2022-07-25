24568d949671e4399a7e10d7b56fefa97874d0de
Arrose <arrose.chen@foxmail.com>
2022-07-25 16:36:37 +0800

    兼容部分浏览器，简化部分接口

--- test/index.js
+++ test/index.js
@@ -6,32 +6,30 @@ async function base64ImageToBuffer(base64String) {
   if (!base64String) {
       return new Error('Invalid base64String');
   }
-  const buffer = new Object()
-  const matches = base64String.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/)
-  buffer.type = matches[1].match(/\/(.*?)$/)[1]
-  buffer.data = Buffer.from(matches[2], 'base64')
-  return buffer
+  const buffer = new Object();
+  const matches = base64String.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+  buffer.type = matches[1].match(/\/(.*?)$/)[1];
+  buffer.data = Buffer.from(matches[2], 'base64');
+  return buffer;
 }
 
 async function writeStreamBufferAsync(path, buffer) {
   return new Promise(async (resolve, reject) => {
-      const writeStream = fs.createWriteStream(path)
+      const writeStream = fs.createWriteStream(path);
       writeStream.on('error', (err) => {
-          console.log("发生异常：", err)
-          return reject(err)
-      })
+          return reject(err);
+      });
       writeStream.on('open', (fd) => {
-          console.log("文件已打开：", fd)
-      })
+          
+      });
       writeStream.on('finish', () => {
-          console.log("写入已完成..")
-          return resolve(path)
-      })
+          return resolve(path);
+      });
       writeStream.on('close', () => {
-          console.log("文件已关闭")
-      })
-      writeStream.write(buffer)
-      writeStream.end()
+      
+      });
+      writeStream.write(buffer);
+      writeStream.end();
   })
 }
 
@@ -50,13 +48,13 @@ async function readIPAInfo() {
 readIPAInfo();
 
 async function readPkgInfoTest() {
-  let ipaInfo = await AppPackageParser.readPkgInfo(path.resolve(__dirname, "test.ipa"), "ipa");
+  let ipaInfo = await AppPackageParser.readPkgInfo(path.resolve(__dirname, "test.ipa"));
   console.log(ipaInfo);
 }
 readPkgInfoTest();
 
 async function readPkgInfoTest() {
-  let apkInfo = await AppPackageParser.readPkgInfo(path.resolve(__dirname, "test.apk"), "apk");
+  let apkInfo = await AppPackageParser.readPkgInfo(path.resolve(__dirname, "test.apk"));
   console.log(apkInfo);
   const iconBuffer = await base64ImageToBuffer(apkInfo.icon);
   await writeStreamBufferAsync(path.resolve(__dirname, `${apkInfo.name}.${iconBuffer.type}`), iconBuffer.data);
